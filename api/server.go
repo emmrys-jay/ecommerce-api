582c01968fa7fd877cc2dd80d4ae614f1fdc96b3
Emmrys-Jay <jonathanemma121@gmail.com>
2022-05-13 02:29:40 +0100

    Start the db and attach all handler func to paths.

--- api/server.go
+++ api/server.go
@@ -2,10 +2,36 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
-	"gopkg.in/mgo.v2"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type Server struct {
-	DB     *mgo.Database
+	DB     *mongo.Database
 	Server *gin.Engine
+}
+
+func StartServer(server *Server, addr string) {
+
+	products := server.Server.Group("/products")
+	{
+		products.POST("/addOne", server.AddOneProduct)
+		products.POST("/add", server.AddProducts)
+		products.GET("/find", server.FindProducts)
+		products.DELETE("/delete", server.DeleteProduct)
+		products.POST("/update", server.UpdateProduct)
+		//products.GET("/categories", getAllCategories)
+	}
+
+	// users := server.Server.Group("/users")
+	//{
+
+	//}
+	// users.POST("/create", server.CreateUser)
+	// users.GET()
+
+	server.Server.Run(addr)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
 }
