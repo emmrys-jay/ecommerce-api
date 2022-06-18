900bbf83b414da614a5c0a25c96570d8d18bb357
Emmrys-Jay <jonathanemma121@gmail.com>
2022-06-18 02:37:01 +0100

    Create createuserrequest struct model

--- db/models.go
+++ db/models.go
@@ -6,13 +6,22 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+// CreateUserRequest models the create user request json body
+type CreateUserRequest struct {
+	Username  string    `json:"username" binding:"required"`
+	Fullname  string    `json:"fullname" binding:"required"`
+	Email     string    `json:"email" binding:"email,required"`
+	Password  string    `json:"password" binding:"required"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
 type User struct {
-	ID             primitive.ObjectID `json:"_id"`
-	Username       string             `json:"username" binding:"required"`
-	HashedPassword string             `json:"password" binding:"required"`
-	Fullname       string             `json:"fullname" binding:"required"`
-	Email          string             `json:"email" binding:"email,required"`
-	CreatedAt      time.Time          `json:"created_at"`
+	ID             primitive.ObjectID `json:"_id" bson:"_id"`
+	Username       string             `json:"username" bson:"username" binding:"required"`
+	HashedPassword string             `json:"password" bson:"password" binding:"required"`
+	Fullname       string             `json:"fullname" bson:"fullname" binding:"required"`
+	Email          string             `json:"email" bson:"email" binding:"email,required"`
+	CreatedAt      time.Time          `json:"created_at" bson:"created_at"`
 }
 
 type Feature struct {
@@ -20,17 +29,17 @@ type Feature struct {
 }
 
 type Product struct {
-	ID          primitive.ObjectID `json:"_id"`
-	Name        string             `json:"name" binding:"required"`
-	Price       float64            `json:"price" binding:"required"`
-	Currency    string             `json:"currency" binding:"required"`
-	Quantity    int64              `json:"quantity" binding:"required"`
-	Description string             `json:"description" binding:"required"`
-	Category    string             `json:"category" binding:"required"`
-	Features    []Feature          `json:"features"`
-	Reviews     []Review           `json:"reviews"`
-	CreatedAt   time.Time          `json:"created_at"`
-	LastUpdated time.Time          `json:"last_updated"`
+	ID          primitive.ObjectID `json:"_id" bson:"_id"`
+	Name        string             `json:"name" bson:"name" binding:"required"`
+	Price       float64            `json:"price" bson:"price" binding:"required"`
+	Currency    string             `json:"currency" bson:"currency" binding:"required"`
+	Quantity    int64              `json:"quantity" bson:"quantity" binding:"required"`
+	Description string             `json:"description" bson:"description" binding:"required"`
+	Category    string             `json:"category" bson:"category" binding:"required"`
+	Features    []Feature          `json:"features" bson:"features"`
+	Reviews     []Review           `json:"reviews" bson:"reviews"`
+	CreatedAt   time.Time          `json:"created_at" bson:"created_at"`
+	LastUpdated time.Time          `json:"last_updated" bson:"last_updated"`
 }
 
 type Review struct {
