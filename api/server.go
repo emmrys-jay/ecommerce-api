907fc3ef13ca8a9c7e7bddeb6d29348f9a61fa96
Emmrys-Jay <jonathanemma121@gmail.com>
2022-06-18 02:45:13 +0100

    Added middleware to handler

--- api/server.go
+++ api/server.go
@@ -1,18 +1,21 @@
 package api
 
 import (
+	"github.com/Emmrys-Jay/ecommerce-api/middleware"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+// Server stores the gin engine and mongo database to be used as method receivers
 type Server struct {
 	DB     *mongo.Database
 	Server *gin.Engine
 }
 
+// StartServer defines all endpoints and associates them with their handlers
 func StartServer(server *Server, addr string) {
 
-	products := server.Server.Group("/products")
+	products := server.Server.Group("/products", middleware.AuthorizeJWT())
 	{
 		products.POST("/addone", server.AddOneProduct)
 		products.POST("/add", server.AddProducts)
@@ -27,11 +30,14 @@ func StartServer(server *Server, addr string) {
 	users := server.Server.Group("/users")
 	{
 		users.POST("/create", server.CreateUser)
+		users.POST("/login", server.LoginUser)
+		users.GET("/get", middleware.AuthorizeJWT(), server.GetAllUsers)
 	}
 
 	server.Server.Run(addr)
 }
 
+// errorResponse returns verbose error response
 func errorResponse(err error) gin.H {
 	return gin.H{"error": err.Error()}
 }
