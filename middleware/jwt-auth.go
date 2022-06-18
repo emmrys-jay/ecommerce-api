772d5685b1a51b59926d127236b78292af21a03a
Emmrys-Jay <jonathanemma121@gmail.com>
2022-06-18 02:35:20 +0100

    Implement simple authorization middleware

---
+++ middleware/jwt-auth.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/Emmrys-Jay/ecommerce-api/token"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthorizeJWT() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		auth := ctx.GetHeader("Authorization")
+		if auth == "" {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"unauthorized": "access denied"})
+			ctx.Abort()
+		}
+
+		jwtToken := strings.Split(auth, " ")[1]
+		tokenMaker, _ := token.NewTokenMaker()
+
+		payload, err := tokenMaker.VerifyToken(jwtToken)
+		if err != nil {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"unauthorized": "access denied"})
+			ctx.Abort()
+		}
+
+		if err := payload.Valid(); err != nil {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"unauthorized": "access denied"})
+			ctx.Abort()
+		}
+	}
+}
