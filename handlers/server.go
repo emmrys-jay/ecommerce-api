d3a45a1ec73f0995831707cbc1a5ad9dc630d274
Emmrys-Jay <jonathanemma121@gmail.com>
2022-07-19 22:48:24 +0100

    Added new paths for users, cart and products

--- handlers/server.go
+++ handlers/server.go
@@ -23,7 +23,7 @@ func StartServer(server *Server, addr string) {
 		products.DELETE("/delete", server.DeleteProduct)
 		products.DELETE("/deleteall", server.DeleteAllProducts)
 		products.PUT("/update", server.UpdateProduct)
-		products.POST("/addreview", server.AddReview)
+		products.PUT("/addreview", server.AddReview)
 		//products.GET("/categories", getAllCategories)
 	}
 
@@ -31,7 +31,23 @@ func StartServer(server *Server, addr string) {
 	{
 		users.POST("/create", server.CreateUser)
 		users.POST("/login", server.LoginUser)
-		users.GET("/get", middleware.AuthorizeJWT(), server.GetAllUsers)
+		users.GET("/getAll", middleware.AuthorizeJWT(), server.GetAllUsers)
+		users.GET("/get", middleware.AuthorizeJWT(), server.GetUser)
+		users.PUT("/password", middleware.AuthorizeJWT(), server.ChangePassword)
+		users.PUT("/update", middleware.AuthorizeJWT(), server.UpdateUserFlexible)
+		users.PUT("/location/add", middleware.AuthorizeJWT(), server.AddLocation)
+		users.DELETE("/delete", middleware.AuthorizeJWT(), server.DeleteUser)
+		users.DELETE("/deleteAll", middleware.AuthorizeJWT(), server.DeleteAllUsers)
+	}
+
+	cart := server.Server.Group("/user/cart", middleware.AuthorizeJWT())
+	{
+		cart.POST("/add", server.AddToCart)
+		cart.DELETE("/delete", server.RemoveFromCart)
+		cart.PUT("/decrement", server.DecrementCartQuantity)
+		cart.GET("/get", server.GetCartItem)
+		cart.GET("/getAll", server.GetUserCartItems)
+		cart.DELETE("/deleteAll", server.DeleteAllCartItems)
 	}
 
 	server.Server.Run(addr)
