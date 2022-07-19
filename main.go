15d52f16bd5dc563fffe9574d96f1bd7d1e80510
Emmrys-Jay <jonathanemma121@gmail.com>
2022-07-19 22:47:12 +0100

    Added code to process .env file

--- main.go
+++ main.go
@@ -4,11 +4,15 @@ import (
 	"github.com/Emmrys-Jay/ecommerce-api/db"
 	handlers "github.com/Emmrys-Jay/ecommerce-api/handlers"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 var server = handlers.Server{}
 
 func main() {
+
+	_ = godotenv.Load("./token/load.env")
+
 	db.ConfigDB()
 	server = handlers.Server{
 		DB:     db.DB,
