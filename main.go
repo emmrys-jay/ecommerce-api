e4219458fa53cbf9fca8e965b09bf5b34008fd26
Emmrys-Jay <jonathanemma121@gmail.com>
2022-07-20 02:02:21 +0100

    Added code to automatically register admin user

--- main.go
+++ main.go
@@ -1,23 +1,75 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/Emmrys-Jay/ecommerce-api/db"
-	handlers "github.com/Emmrys-Jay/ecommerce-api/handlers"
+	"github.com/Emmrys-Jay/ecommerce-api/endpoints"
+	"github.com/Emmrys-Jay/ecommerce-api/entity"
+	"github.com/Emmrys-Jay/ecommerce-api/middleware"
+	"github.com/Emmrys-Jay/ecommerce-api/repository"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
-
-var server = handlers.Server{}
 
 func main() {
 
-	_ = godotenv.Load("./token/load.env")
+	_ = godotenv.Load("load.env")
 
-	db.ConfigDB()
-	server = handlers.Server{
-		DB:     db.DB,
-		Server: gin.Default(),
+	database := db.ConfigDB()
+
+	adminUsername := os.Getenv("ADMIN_USERNAME")
+	if adminUsername == "" {
+		log.Fatalln("admin username not specified")
 	}
 
-	handlers.StartServer(&server, ":8080")
+	adminPassword := os.Getenv("ADMIN_PASSWORD")
+	if adminPassword == "" {
+		log.Fatalln("admin password not specified")
+	}
+
+	_, err := repository.GetUser(db.GetCollection(database, "user"), adminUsername)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			admin := entity.User{
+				ID:             primitive.NewObjectID(),
+				Username:       adminUsername,
+				PasswordSalt:   "ADMIN",
+				HashedPassword: adminPassword,
+				Fullname:       "ADMIN",
+				Email:          "ADMIN",
+			}
+
+			_, err := repository.CreateUser(db.GetCollection(database, "user"), admin)
+			if err != nil {
+				log.Fatalln("could not create admin user")
+			}
+		} else {
+			log.Fatalln("something went wrong")
+		}
+	}
+
+	// server := gin.New()
+	// server.Use()
+
+	server := gin.Default()
+
+	adminMdw := middleware.AuthorizeAdmin(adminUsername)
+
+	userMdw := middleware.AuthorizeJWT()
+
+	// Initialize endpoints
+	endpoints.InitializeAdminEndpoints(database, server, adminMdw)
+	endpoints.InitializeCartEndpoints(database, server, userMdw)
+	endpoints.InitializeUserEndpoints(database, server, userMdw)
+	endpoints.InitializeProductEndpoints(database, server, userMdw)
+
+	err = server.Run()
+	if err != nil {
+		log.Fatalln("could not start server")
+	}
+
 }
