7790e907918ed20cadbfe70180c0ddad429cb58e
Emmrys-Jay <jonathanemma121@gmail.com>
2022-08-05 09:25:19 +0100

    Added util function to return username from token

---
+++ util/token.go
@@ -0,0 +1,25 @@
+package util
+
+import (
+	"strings"
+
+	auth "github.com/Emmrys-Jay/ecommerce-api/auth/jwt"
+	"github.com/gin-gonic/gin"
+)
+
+func UsernameFromToken(ctx *gin.Context) (string, error) {
+	tokenMaker, err := auth.NewTokenMaker()
+	if err != nil {
+		return "", err
+	}
+
+	// Get Authorization header and split it to get JWT token
+	// Verify JWT token to get custom payload which contains username information
+	tokenString := strings.Split(ctx.GetHeader("Authorization"), " ")[1]
+	payload, err := tokenMaker.VerifyToken(tokenString)
+	if err != nil {
+		return "", err
+	}
+
+	return payload.Username, nil
+}
