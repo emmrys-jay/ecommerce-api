8b9daf4718251e73b1fe925201d69f25502633d8
Emmrys-Jay <jonathanemma121@gmail.com>
2022-08-22 15:31:03 +0100

    Add user and userid functions-from-token

--- util/token.go
+++ util/token.go
@@ -4,7 +4,10 @@ import (
 	"strings"
 
 	auth "github.com/Emmrys-Jay/ecommerce-api/auth/jwt"
+	"github.com/Emmrys-Jay/ecommerce-api/entity"
+	"github.com/Emmrys-Jay/ecommerce-api/repository"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func UsernameFromToken(ctx *gin.Context) (string, error) {
@@ -22,4 +25,43 @@ func UsernameFromToken(ctx *gin.Context) (string, error) {
 	}
 
 	return payload.Username, nil
+}
+
+func UserIDFromToken(ctx *gin.Context) (string, error) {
+	tokenMaker, err := auth.NewTokenMaker()
+	if err != nil {
+		return "", err
+	}
+
+	// Get Authorization header and split it to get JWT token
+	// Verify JWT token to get custom payload which contains username information
+	tokenString := strings.Split(ctx.GetHeader("Authorization"), " ")[1]
+	payload, err := tokenMaker.VerifyToken(tokenString)
+	if err != nil {
+		return "", err
+	}
+
+	return payload.ID, nil
+}
+
+func UserFromToken(ctx *gin.Context, collection *mongo.Collection) (*entity.User, error) {
+	tokenMaker, err := auth.NewTokenMaker()
+	if err != nil {
+		return nil, err
+	}
+
+	// Get Authorization header and split it to get JWT token
+	// Verify JWT token to get custom payload which contains username information
+	tokenString := strings.Split(ctx.GetHeader("Authorization"), " ")[1]
+	payload, err := tokenMaker.VerifyToken(tokenString)
+	if err != nil {
+		return nil, err
+	}
+
+	user, err := repository.GetUser(collection, payload.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
